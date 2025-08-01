781f92c2778d5f4ed60a3e8d29685ed0804bb618
Mahika Lakoul <mahikalakoul01@gmail.com>
2025-08-01 10:41:56 +0545

    Update settings.py

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-hehawc&m)8nlhv=#3rewl(gg58sh+vbb6r_b^s4h$lgbxie7hx
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'sumerclass.onrender.com',
+    'https://sumerclass.onrender.com',
     '127.0.0.1',
     'localhost',
 ]
